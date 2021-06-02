23b6ffe70120fd8583ae17bbed65a4876dbecf9e
윤현준 <homelus@daum.net>
2021-06-03 01:21:36 +0900

    Limit and Streamable 추가

--- src/main/java/study/together/example/jun/repository/BookRepository.java
+++ src/main/java/study/together/example/jun/repository/BookRepository.java
@@ -1,6 +1,11 @@
 package study.together.example.jun.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.util.Streamable;
 import study.together.example.jun.model.Book;
 
 import java.util.List;
@@ -24,4 +29,21 @@ public interface BookRepository extends MongoRepository<Book, String> {
     List<Book> findAllByOrderByPriceDesc();
 
     List<Book> findByAuthor_Name(String name);
+
+    /* LIMIT */
+    Book findFirstByOrderByNameAsc();
+
+    Book findTopByOrderByNameDesc();
+
+    Page<Book> queryFirst3ByTitle(String title, Pageable pageable);
+
+    Slice<Book> findTop3ByTitle(String title, Pageable pageable);
+
+    List<Book> findFirst2ByTitle(String title, Sort sort);
+
+    List<Book> findTop2ByTitle(String title, Pageable pageable);
+
+    /* Collection */
+    Streamable<Book> findByTitleContaining(String title);
+
 }
